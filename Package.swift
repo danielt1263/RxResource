72a64589082cef4955b14f5ced39ae4bd38a8863
Daniel Tartaglia <danielt1263@gmail.com>
2022-05-27 18:41:01 -0400

    Add platform requirements.

--- Package.swift
+++ Package.swift
@@ -1,12 +1,13 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RxResource",
+	platforms: [
+		.iOS(.v9),
+	],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RxResource",
             targets: ["RxResource"]),
@@ -18,8 +19,6 @@ let package = Package(
 		)
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RxResource",
 			dependencies: [
